02c3a0fad41eb1e5cdc7f6e201dfcdd961e85b9d
Your Name <you@example.com>
2022-09-17 17:06:00 +0900

    gitignore

--- accountapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-from accountapp.views import hello_world_drf
-
-urlpatterns = [
-    path('hello_world_drf/', hello_world_drf.as_view()),
-]
--- accountapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-
-# Create your views here.
-class hello_world_drf(APIView):
-    def get(self, request):
-        return Response({'message':"hello_world_drf!"})
