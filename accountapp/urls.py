d2a04184dee8e6adbd71aac07ca234c2328a792a
Your Name <you@example.com>
2022-09-27 00:06:18 +0900

    gitignore

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('user_list',UserListView.as_view()),
     path('user_list/<int:user_id>',UserDetailView.as_view()),
     path('rest_auth/register',include('dj_rest_auth.registration.urls')),
-    path('rest_auth', include('dj_rest_auth.urls')),
+    path('rest-auth/', include('dj_rest_auth.urls')),
 ]
--- drf/settings.py
+++ drf/settings.py
@@ -12,18 +12,24 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os, environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    DEBUG=(bool,True)
+)
+
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
+
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qvjolnl263glp24*$^rhkfmp^y*tqu67y@mhlo#&$=4q@fx%io'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -139,3 +145,9 @@ REST_FRAMEWORK ={
         'rest_framework.permissions.IsAuthenticated'
     ],
 }
+
+REST_USE_JWT = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
+SITE_ID = 1
